0408ad3ddeb9d8c69fb0bbaff3bc1dfda84c1886
Aneliya Boneva <132662622+AneliyaYaYaYa@users.noreply.github.com>
2023-11-08 15:29:57 +0200

    merging verify methods into BasePage

--- LambdatestChallenge/src/main/java/pages/BasePage.java
+++ LambdatestChallenge/src/main/java/pages/BasePage.java
@@ -1,6 +1,11 @@
 package pages;
 
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static core.Utils.LOGGER;
 
 public  class BasePage {
     public String url;
@@ -14,4 +19,14 @@ public  class BasePage {
         driver.get(url);
     }
 
+    public void verifyHeader(String message) {
+        WebElement successTitle = driver.findElement(By.xpath("//div/h1[contains(@class, 'page-title')]"));
+        String value = successTitle.getAttribute("innerText");
+        Assertions.assertTrue(value.contains(message), "Header title not as expected.");
+    }
+    public void verifyPageNavigated(String expectedUrl) {
+        String actualUrl=driver.getCurrentUrl();
+        Assertions.assertEquals(expectedUrl,actualUrl, "Expected page was not navigated.");
+        LOGGER.info("Page was successfully navigated.");
+    }
 }
